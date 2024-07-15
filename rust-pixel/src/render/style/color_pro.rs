a223e31b98f7f016bcf7746d3cd8e1eca8d74da5
zipxing <zipxing@hotmail.com>
2024-07-16 07:20:47 +0800

    coding color

--- rust-pixel/src/render/style/color_pro.rs
+++ rust-pixel/src/render/style/color_pro.rs
@@ -3,6 +3,7 @@ use std::fmt;
 use std::ops::{Index, IndexMut};
 use ColorSpace::*;
 
+#[derive(Debug, Clone, Copy)]
 pub enum ColorSpace {
     SRGBA,
     LinearRGBA,
@@ -489,3 +490,119 @@ fn cmyk_to_srgba(cmyk: ColorData) -> ColorData {
     [r, g, b, 1.0]
 }
 
+fn interpolate(a: f64, b: f64, fra: Fraction) -> f64 {
+    a + fra.value() * (b - a)
+}
+
+fn mix(c1: ColorData, c2: ColorData, fra: Fraction) -> ColorData {
+    [
+        interpolate(c1[0], c2[0], fra),
+        interpolate(c1[1], c2[1], fra),
+        interpolate(c1[2], c2[2], fra),
+        interpolate(c1[3], c2[3], fra),
+    ]
+}
+
+pub fn clamp(lower: f64, upper: f64, x: f64) -> f64 {
+    f64::max(f64::min(upper, x), lower)
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Fraction {
+    f: f64,
+}
+
+impl Fraction {
+    pub fn from(s: f64) -> Self {
+        Fraction {
+            f: clamp(0.0, 1.0, s),
+        }
+    }
+
+    pub fn value(self) -> f64 {
+        self.f
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ColorStop {
+    color: ColorPro,
+    position: Fraction,
+}
+
+#[derive(Debug, Clone)]
+pub struct ColorScale {
+    color_stops: Vec<ColorStop>,
+}
+
+impl ColorScale {
+    pub fn empty() -> Self {
+        Self {
+            color_stops: Vec::new(),
+        }
+    }
+
+    pub fn add_stop(&mut self, color: ColorPro, position: Fraction) -> &mut Self {
+        #![allow(clippy::float_cmp)]
+        let same_position = self
+            .color_stops
+            .iter_mut()
+            .find(|c| position.value() == c.position.value());
+
+        match same_position {
+            Some(color_stop) => color_stop.color = color,
+            None => {
+                let next_index = self
+                    .color_stops
+                    .iter()
+                    .position(|c| position.value() < c.position.value());
+
+                let index = next_index.unwrap_or(self.color_stops.len());
+
+                let color_stop = ColorStop { color, position };
+
+                self.color_stops.insert(index, color_stop);
+            }
+        };
+
+        self
+    }
+
+    pub fn sample(
+        &self,
+        position: Fraction,
+        cs: ColorSpace,
+    ) -> Option<ColorData> {
+        if self.color_stops.len() < 2 {
+            return None;
+        }
+
+        let left_stop = self
+            .color_stops
+            .iter()
+            .rev()
+            .find(|c| position.value() >= c.position.value());
+
+        let right_stop = self
+            .color_stops
+            .iter()
+            .find(|c| position.value() <= c.position.value());
+
+        match (left_stop, right_stop) {
+            (Some(left_stop), Some(right_stop)) => {
+                let diff_color_stops = right_stop.position.value() - left_stop.position.value();
+                let diff_position = position.value() - left_stop.position.value();
+                let local_position = Fraction::from(diff_position / diff_color_stops);
+
+                let color = mix(
+                    left_stop.color[cs].unwrap(),
+                    right_stop.color[cs].unwrap(),
+                    local_position,
+                );
+
+                Some(color)
+            }
+            _ => None,
+        }
+    }
+}
