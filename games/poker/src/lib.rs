4751b49178da48ed071a70d344609da98cbe9caf
zipxing <zipxing@hotmail.com>
2024-10-04 17:24:47 +0800

    fix poker ginrummy lib.rs

--- games/poker/src/lib.rs
+++ games/poker/src/lib.rs
@@ -1,15 +1,6 @@
 mod model;
 mod render;
 
-use crate::{model::PokerModel, render::PokerRender};
-use rust_pixel::game::Game;
-
-#[cfg(target_arch = "wasm32")]
-use rust_pixel::render::adapter::web::{input_events_from_web, WebAdapter};
-#[cfg(target_arch = "wasm32")]
-use rust_pixel::render::adapter::RenderCell;
-use wasm_bindgen::prelude::*;
-
 use pixel_macro::pixel_game;
 
 pixel_game!(Poker);
