d5e394c0b5383b29522cce8e4e52fefae1d633df
zipxing <zipxing@hotmail.com>
2024-10-16 15:42:55 +0800

    update

--- rust-pixel/cargo-pixel/src/main.rs
+++ rust-pixel/cargo-pixel/src/main.rs
@@ -59,7 +59,7 @@ fn common_arg(app: App) -> App {
 
 fn make_parser() -> ArgMatches {
     let matches = App::new("cargo pixel")
-        .version("0.5.1")
+        .version("0.5.2")
         .author("zipxing@hotmail.com")
         .about("RustPixel cargo build tool")
         .arg(Arg::with_name("pixel"))
@@ -463,7 +463,7 @@ fn check_pixel_toml() -> PixelContext {
         }
         if let Some(cargo_pixel) = pixel.get("cargo_pixel") {
             let cps = cargo_pixel.to_string();
-            if cps != "\"0.5.1\"" {
+            if cps != "\"0.5.2\"" {
                 panic!("Please update cargo pixel: cargo install --path tools/cargo-pixel --root ~/.cargo");
             }
         }
